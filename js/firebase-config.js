1449cf9ba61429aa1591002742f6758379a1e9a0
VirtualStudios <ceovirtualstudios@gmail.com>
2026-02-04 11:21:38 -0400

    Actualizado el mié 02/04/2026 11:21

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDFn7fJPpOzuyiBKBXh7Lm8pHN6TwY8K-g",
   authDomain: "virtualgift-login.firebaseapp.com",
   projectId: "virtualgift-login",
-  storageBucket: "virtualgift-login.firebasestorage.app",
+  storageBucket: "virtualgift-login.appspot.com",
   messagingSenderId: "807245369735",
   appId: "1:807245369735:web:b52a8412bfb23c8ad28322",
   measurementId: "G-LF2SDF6J90"
